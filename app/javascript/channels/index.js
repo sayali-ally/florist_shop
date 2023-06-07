727db0db3bde01355b86b7159403df1e55f6b5fa
sayali-ally <85214077+sayali-ally@users.noreply.github.com>
2023-06-07 11:04:48 +0530

    Update index.js

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,5 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
-
+heyyyyyyyyyyyyyyyyyyyyyyyyyyy
 const channels = require.context('.', true, /_channel\.js$/)
 
